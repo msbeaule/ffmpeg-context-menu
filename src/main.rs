c897b08ca7b42af54797d71e163d32cfdb838d40
Michelle <msbeaule@gmail.com>
2024-02-10 17:18:51 -0800

    added convert to no audio, fix video, and convert to 265

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,10 @@ fn main() {
         "half_size" => convert_to_new_half_size(&args.path),
         "audio" => convert_to_audio_only(&args.path),
         "mp4" => convert_to_mp4(&args.path),
+        "no_audio" => convert_to_no_audio(&args.path),
+        "fix" => fix_video(&args.path),
+        "265" => convert_to_265(&args.path),
+        //"remove_border" => find_and_remove_black_borders(&args.path),
         _=> println!("wrong option entered, refer to README"),
     };
 
@@ -97,10 +101,11 @@ fn convert_to_new_half_size(original_full_path: &std::path::PathBuf) {
     let _result = child.wait().unwrap();
 }
 
+/// Converts to audio only as a .mp3
 fn convert_to_audio_only(original_full_path: &std::path::PathBuf) {
     let (path, file_name, _) = get_full_path_parts(original_full_path);
 
-    let new_name = format!("{}/{}-audio.mp3", path.to_str().unwrap(), file_name.to_str().unwrap());
+    let new_name = format!("{}/{}.mp3", path.to_str().unwrap(), file_name.to_str().unwrap());
 
     let mut child = Command::new("ffmpeg")
         .arg("-i")
@@ -115,6 +120,7 @@ fn convert_to_audio_only(original_full_path: &std::path::PathBuf) {
     let _result = child.wait().unwrap();
 }
 
+/// Makes a new video file in the .mp4 format
 fn convert_to_mp4(original_full_path: &std::path::PathBuf) {
     let (path, file_name, _) = get_full_path_parts(original_full_path);
 
@@ -125,6 +131,74 @@ fn convert_to_mp4(original_full_path: &std::path::PathBuf) {
         .arg(original_full_path)
         .arg("-f")
         .arg("mp4")
+        .arg(new_name)
+        .spawn()
+        .unwrap();
+
+    let _result = child.wait().unwrap();
+}
+
+/// Makes a new video file with the copied video but no audio
+fn convert_to_no_audio(original_full_path: &std::path::PathBuf) {
+    let (path, file_name, extension) = get_full_path_parts(original_full_path);
+
+    let new_name = format!("{}/{}-NO-AUDIO.{}", path.to_str().unwrap(), file_name.to_str().unwrap(), extension.to_str().unwrap());
+
+    let mut child = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(original_full_path)
+        .arg("-map")
+        .arg("0:v")
+        .arg("-map_metadata")
+        .arg("0")
+        .arg("-crf")
+        .arg("28")
+        .arg(new_name)
+        .spawn()
+        .unwrap();
+
+    let _result = child.wait().unwrap();
+}
+
+/// Copies the original video, repairing it if needed by ignoring errors
+fn fix_video(original_full_path: &std::path::PathBuf) {
+    let (path, file_name, extension) = get_full_path_parts(original_full_path);
+
+    let new_name = format!("{}/{}-FIXED.{}", path.to_str().unwrap(), file_name.to_str().unwrap(), extension.to_str().unwrap());
+
+    let mut child = Command::new("ffmpeg")
+        .arg("-err_detect")
+        .arg("ignore_err")
+        .arg("-i")
+        .arg(original_full_path)
+        .arg("-c")
+        .arg("copy")
+        .arg(new_name)
+        .spawn()
+        .unwrap();
+
+    let _result = child.wait().unwrap();
+}
+
+/// Converts video to 265 format
+fn convert_to_265(original_full_path: &std::path::PathBuf) {
+    let (path, file_name, extension) = get_full_path_parts(original_full_path);
+
+    let new_name = format!("{}/{}-h.265.{}", path.to_str().unwrap(), file_name.to_str().unwrap(), extension.to_str().unwrap());
+
+    let mut child = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(original_full_path)
+        .arg("-c:v")
+        .arg("libx265")
+        .arg("-map_metadata")
+        .arg("0")
+        .arg("-vtag")
+        .arg("hvc1")
+        .arg("-c:a")
+        .arg("copy")
+        .arg("-vcodec")
+        .arg("hevc_nvenc")
         .arg(new_name)
         .spawn()
         .unwrap();
